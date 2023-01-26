b00744c871bec150bade0a2817726bf47e7a011e
BigNovik <113912814+BigNovik@users.noreply.github.com>
2023-01-26 09:18:17 +0200

    more code

--- src/components/MyBooks.jsx
+++ src/components/MyBooks.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 
 export const MyBooks = () => {
-    const handleMyBooks = (e) => {}
+    const handleMyBooks = () => {}
   return (
     <div>
-      <Form className="d-flex w-75 m-auto mt-5 mb-5">
+      <h1 className="mt-3">My Books</h1>
+      <Form className="d-flex w-75 m-auto mt-3 mb-5">
         <Form.Select onChange={handleMyBooks} className="me-2" aria-label="Default select example">
           <option value="currently">Books I currently read:</option>
           <option value="finished">Books I have finished reading:</option>
